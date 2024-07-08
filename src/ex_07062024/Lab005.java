feb00ba47a472b96319a0041cc347da34fa1b56d
nagakln116 <tester.naga257338@gmail.com>
2024-07-08 14:02:14 +0530

    Added new files

---
+++ src/ex_07062024/Lab005.java
@@ -0,0 +1,10 @@
+package ex_07062024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        System.out.print(2+2);
+        System.out.print(2*2);
+        System.out.print(2-2);
+        System.out.print(10/1);
+    }
+}
