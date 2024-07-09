634b08615944b11d9c4a922f814329ddbb114551
nagakln116 <tester.naga257338@gmail.com>
2024-07-09 10:19:13 +0530

    Added new files

---
+++ src/ex_07072024/Lab012.java
@@ -0,0 +1,8 @@
+package ex_07072024;
+
+public class Lab012 {
+    static public void main(String[] a) {
+        System.out.println("Interview Qs");
+
+    }
+}
---
+++ src/ex_07072024/Lab013.java
@@ -0,0 +1,5 @@
+package ex_07072024;
+
+public class Lab013 {
+    //Class without main method
+}
