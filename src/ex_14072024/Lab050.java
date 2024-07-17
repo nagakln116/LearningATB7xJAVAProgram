177136e9e7dc3cc4e28feeb96d47fe6a5cad84ee
nagakln116 <tester.naga257338@gmail.com>
2024-07-17 22:35:44 +0530

    Added new files

---
+++ src/ex_14072024/Lab050.java
@@ -0,0 +1,11 @@
+package ex_14072024;
+
+public class Lab050 {
+    public static void main(String[] args) {
+        //Operators
+        int age = 65; //Unary operator is =
+        //datatype - variable - assignment operator - Operand
+        // 3 types of operator; Unary, Binary & ternary operators
+        // Unary needs single operand; Binary needs two operands; ternary needs three operands
+    }
+}
---
+++ src/ex_14072024/Lab051.java
@@ -0,0 +1,14 @@
+package ex_14072024;
+
+public class Lab051 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int c = a + b;
+        System.out.println(c);
+        //Arithmetic operator + - * / %
+        //Compound operators +=; -+; /=; *=;
+        //Relational operators >; <; <=; >=; !; !=; == (compare two value)
+        //Logical operator OR; AND; NOT;
+    }
+}
