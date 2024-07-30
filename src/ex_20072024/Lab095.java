003d23cf834b8136e449aed23f2b844a3b0b73d3
nagakln116 <tester.naga257338@gmail.com>
2024-07-30 22:54:55 +0530

    Added new files

---
+++ src/ex_20072024/Lab095.java
@@ -0,0 +1,26 @@
+package ex_20072024;
+
+import java.util.Scanner;
+
+public class Lab095 {
+    public static void main(String[] args) {
+        //Take an input char from user and identify if it is a vowel
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter the character :");
+        char user_input = sc.next().charAt(0);
+        switch (user_input){
+            case 'a': System.out.println("vowel");
+            break;
+            case 'e': System.out.println("vowel");
+            break;
+            case 'i': System.out.println("vowel");
+            break;
+            case 'o': System.out.println("vowel");
+            break;
+            case 'u': System.out.println("vowel");
+            break;
+            default: System.out.println("not a vowel");
+        }
+
+    }
+}
