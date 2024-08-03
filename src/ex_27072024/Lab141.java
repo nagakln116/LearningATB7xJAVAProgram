2b55aabd9904e13b7220e499cbbac4726212f805
nagakln116 <tester.naga257338@gmail.com>
2024-08-03 13:31:50 +0530

    Added new files

---
+++ src/ex_27072024/Lab141.java
@@ -0,0 +1,9 @@
+package ex_27072024;
+
+public class Lab141 {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        a[2] = 90;
+        System.out.println(a[1]); // it will be 0 as the value is not initialized yet
+    }
+}
