c572c9f8ea1565cc0db2033a458cfd8c369526b6
nagakln116 <tester.naga257338@gmail.com>
2024-08-05 09:47:55 +0530

    Added new files

---
+++ src/ex_20072024/Lab099.java
@@ -0,0 +1,16 @@
+package ex_20072024;
+
+import java.util.Scanner;
+
+public class Lab099 {
+    public static void main(String[] args) {
+        //Break statement is replaced by ->
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter the character :");
+        char user_input = sc.next().charAt(0);
+        switch (user_input) {
+            case 'a', 'e', 'i', 'o', 'u' -> System.out.println("vowel");
+            default -> System.out.println("not a vowel");
+        }
+    }
+}
